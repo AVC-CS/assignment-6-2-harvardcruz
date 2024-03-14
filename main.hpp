45050a1f46ddb0b3d6457f501ee2e03b8e1df769
harvardcruz <harvard.cruz8@gmail.com>
2024-03-14 15:44:05 -0700

    :)

--- main.hpp
+++ main.hpp
@@ -13,13 +13,43 @@ int getClosestMed(int, int, int);
 // complete two functions
 int getRandom(void)
 {
+        int V;
+
+        V = rand() % 100;
+
+        return V;
+
+
         /***************************************************
          * Code your program here
          ***************************************************/
 }
 int getClosestMed(int rdnum1, int rdnum2, int rdnum3)
 {
+
+        int med;
+
+        if((rdnum1 < rdnum2) && (rdnum1 > rdnum3)){
+                med = rdnum1;
+        }
+        else if ((rdnum2 < rdnum1) && (rdnum2 > rdnum3)){
+                med = rdnum2;
+        }
+        else if ((rdnum3 < rdnum1) && (rdnum3 > rdnum2)){
+                med = rdnum3;
+        }
+        else if ((rdnum1 > rdnum2) && (rdnum1 < rdnum3)){
+                med = rdnum1;
+        }
+        else if ((rdnum2 > rdnum1) && (rdnum2 < rdnum3)){
+                med = rdnum2;
+        }
+        else if ((rdnum3 > rdnum1) && (rdnum3 < rdnum2)){
+                med = rdnum3;
+        }
+        return med;
         /***************************************************
          * Code your program here
          ***************************************************/
+        
 }
